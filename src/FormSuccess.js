36e40db20dcca2a9658130763cba86418b31052f
Kishanrayanal <68189912+kishanrayanal@users.noreply.github.com>
2021-07-18 23:47:47 +0530

    first

---
+++ src/FormSuccess.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Form.css';
+
+const FormSuccess = () => {
+  return (
+    <div className='form-content-right'>
+      <h1 className='form-success'>Welcome to HiCoder!</h1>
+      <h2 className='form-success'>HiCoder does everything that helps students become successful in the software industry. From product thinking to product designing, system designing, choosing the right frameworks, breaking projects in stages to building teams, and working collaboratively to making the application comply with the latest Google SEO needs and knowledge about integrated analytics, we do everything in between.</h2>
+      
+    </div>
+  );
+};
+
+export default FormSuccess;
